d441f8b7737a258f186041d008a1c8d8b259f6a0
Ivana Banchero <ivana.banchero7@gmail.com>
2025-04-19 21:34:38 -0300

    Add docstring and comments

--- app.py
+++ app.py
@@ -5,11 +5,23 @@ import pandas as pd
 
 
 def create_catalogue(url: str, html_object: str) -> list:
+    """
+    Function:
+    Scrapes the requested URL and creates arrays with each work's information from the catalogue.
+
+    Args:
+    url (str): The requested URL.
+    html_object (str): The type of HTML objects to fetch.
+
+    Returns:
+    list: A list containing lists with each work's details in order.
+    """
     res = requests.get(url)
     soup = BeautifulSoup(res.text, 'html.parser')
 
     data_cells = soup.find_all(html_object)
 
+    # Fetch row-wise each detail based on the first letter of each HTML object's unique identifier
     numbers = [cell.getText().strip() for cell in data_cells if cell.get('data-cell-id').startswith('B')]
     titles = [cell.getText().strip() for cell in data_cells if cell.get('data-cell-id').startswith('C')]
 
@@ -17,13 +29,15 @@ def create_catalogue(url: str, html_object: str) -> list:
     for cell in data_cells:
         if cell.get('data-cell-id').startswith('D'):
             key = cell.getText().strip()
-            keys.append(key) if key else keys.append('N/A')
+            keys.append(key) if key else keys.append('N/A') # Replace blank values with 'N/A'
     
     locations = [cell.getText().strip() for cell in data_cells if cell.get('data-cell-id').startswith('F')]
-    years = [int(cell.getText()) for cell in data_cells if cell.get('data-cell-id').startswith('E')]
-    genres = [re.findall(r'(?<=\s).*$', cell.getText().strip())[0] for cell in data_cells if cell.get('data-cell-id').startswith('G')]
+    years = [cell.getText().strip() for cell in data_cells if cell.get('data-cell-id').startswith('E')]
 
-    # Use the enumerate() generator with one of the lists ('numbers' in this case) to create an index parameter for the other ones
+    genres = [re.findall(r'(?<=\s).*$', cell.getText().strip())[0] # Filter from each work's genre the text without initials, signs or numbers
+              for cell in data_cells if cell.get('data-cell-id').startswith('G')]
+
+    # Use enumerate() with one of the lists ('numbers' in this case) to create an index parameter for the other ones
     catalogue = [[item, titles[idx], keys[idx], locations[idx], years[idx], genres[idx]] for idx, item in enumerate(numbers)]
   
     return catalogue
@@ -32,8 +46,9 @@ def create_catalogue(url: str, html_object: str) -> list:
 if __name__ == '__main__':
     url = 'https://allaboutmozart.com/mozart-kochel-catalogue-works-compositions-koechel/'
     catalogue = create_catalogue(url, 'td')
-    sorted_catalogue = sorted(catalogue, key=lambda x: (x[0], x[4])) # Sort by number and year
+    sorted_catalogue = sorted(catalogue, key=lambda x: (x[0], x[4])) # Sort rows by number and year
 
+    # Write the .csv file using the sorted catalogue and a specified header
     file_name = 'catalogue.csv'
     header = ['number', 'title', 'key', 'location', 'year', 'genre']
     df = pd.DataFrame(sorted_catalogue, columns=header)
